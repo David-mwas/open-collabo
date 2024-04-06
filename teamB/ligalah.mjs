c711938517f0bf6ce71cbb9b884219443f4918a2
krillitane <ligalahhezron@gmail.com>
2024-04-06 02:23:53 -0700

    Add : add my details

---
+++ teamB/ligalah.mjs
@@ -0,0 +1,18 @@
+const yourNameData = {
+    teamName: "Team B",
+    firstname: "Ligalah",
+    lastname: "Hezron",
+    email: "ligahh@gmail.com",
+    role: "Developer",
+    skills: ["HTML", "CSS", "JavaScript", "Angular"],
+    socials: [
+      {
+        git: "Ligalah",
+      },
+      {
+        twitter: "https://twitter.com/ligalahh",
+      },
+    ],
+  };
+  
+  export default ligalaData;
