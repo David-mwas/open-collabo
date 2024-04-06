63988cd8b4e0a213e437c3f92a3d0675cf209801
willington juma <willingtonejumamconyango@gmail.com>
2024-04-06 13:17:36 +0300

    Add: add my details

--- names.mjs
+++ names.mjs
@@ -1,5 +1,6 @@
 import davidData from "./teamA/david.mjs";
+import jumadata from "./teamA/jumas.mjs";
 import someoneData from "./teamB/someone.mjs";
 
-const data = [davidData, someoneData];
+const data = [davidData, someoneData,jumadata];
 export default data;
---
+++ teamA/jumas.mjs
@@ -0,0 +1,18 @@
+const jumadata = {
+    teamName: " A",
+    firstname: "Willington Juma",
+    lastname: "Your Last Name",
+    email: "willingtonejumamconyango@gmail.com",
+    role: "Backended developer",
+    skills: ["HTMl ", "Django", "python"],
+    socials: [
+      {
+        git: "samrato",
+      },
+      {
+        twitter: "sam_rato",
+      },
+    ],
+  };
+  
+  export default jumadata;
