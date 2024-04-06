bb1e6dd9415a002f5a0a8978141e7702ebb9c78c
Amianie <amianielvis8@gmail.com>
2024-04-06 12:15:38 +0300

    Added Amiani.

--- names.mjs
+++ names.mjs
@@ -1,5 +1,6 @@
 import davidData from "./teamA/david.mjs";
 import someoneData from "./teamB/someone.mjs";
+import AmianiData from "./teamA/Amiani.mjs";
 
-const data = [davidData, someoneData];
+const data = [davidData, someoneData,AmianiData];
 export default data;
