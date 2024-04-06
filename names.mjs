ee9849de565aee873891422af4d90fcb11db067a
kevianto <kevinshahale746@gmail.com>
2024-04-06 12:26:03 +0300

    Add:add my details in kevin.mjs add kevindata object

--- names.mjs
+++ names.mjs
@@ -1,5 +1,6 @@
 import davidData from "./teamA/david.mjs";
+import KevinData from "./teamB/kevin.mjs";
 import someoneData from "./teamB/someone.mjs";
 
-const data = [davidData, someoneData];
+const data = [davidData, someoneData,KevinData];
 export default data;
