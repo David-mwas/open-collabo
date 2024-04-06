6cad80c8a8efeeae76916cfbd13be0f5933b7495
Kibs-Neville <nkibwanga@gmail.com>
2024-04-06 12:21:33 +0300

    Add : add my details in neville.mjs, sebbie.mjs, add data objects in names.mjs

--- names.mjs
+++ names.mjs
@@ -1,5 +1,7 @@
 import davidData from "./teamA/david.mjs";
+import nevilleData from "./teamA/neville.mjs";
+import SebbieData from "./teamA/sebbie.mjs";
 import someoneData from "./teamB/someone.mjs";
 
-const data = [davidData, someoneData];
+const data = [davidData, someoneData,nevilleData,SebbieData];
 export default data;
---
+++ teamA/neville.mjs
@@ -0,0 +1,18 @@
+const nevilleDataData = {
+    teamName: "team A",
+    firstname: "Neville",
+    lastname: "Evan",
+    email: "nkibwanga@gmail.com",
+    role: "Data Scientist",
+    skills: ["Python", "MySQL", "NumPy"],
+    socials: [
+      {
+        git: "Kibs-Neville",
+      },
+      {
+        twitter: "Kibs_Neville",
+      },
+    ],
+  };
+  
+  export default nevilleData;
