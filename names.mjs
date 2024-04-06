dbe510458bb1d948efb7139a95fa5077fcc30613
Elvis Amiani Liuva <83879920+Amianie@users.noreply.github.com>
2024-04-06 12:52:07 +0300

    Update names.mjs

--- names.mjs
+++ names.mjs
@@ -1,5 +1,5 @@
 import davidData from "./teamA/david.mjs";
 import someoneData from "./teamB/someone.mjs";
 import otipaData from "./teamB/otipa.mjs";
-const data = [davidData, someoneData];
+const data = [davidData, someoneData,otipaData];
 export default data;
