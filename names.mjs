6cd6f7c04b5501b0bbdaa0d92e14e8f4576868b9
GitMaster254 <sitb01-029072022@student.mmust.ac.ke>
2024-04-06 12:24:05 +0300

    add: add my details in Silfanus.mjs,add object Silfanusdata in names.mjs

--- names.mjs
+++ names.mjs
@@ -1,5 +1,6 @@
 import davidData from "./teamA/david.mjs";
+import SilfanusData from "./teamB/Silfanus.mjs";
 import someoneData from "./teamB/someone.mjs";
 
-const data = [davidData, someoneData];
+const data = [davidData, someoneData, SilfanusData];
 export default data;
