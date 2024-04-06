3248a81b1e2d77320e29fdc3f1beecc7489f6bce
Calvinmuemah <kelvinmuemah855@gmail.com>
2024-04-06 12:24:03 -0700

    Add :add my details in Calvin.mjs, add CalvinData object in data list in names.mjs

--- names.mjs
+++ names.mjs
@@ -1,5 +1,6 @@
+import CalvinData from "./teamA/calvin.mjs";
 import davidData from "./teamA/david.mjs";
 import someoneData from "./teamB/someone.mjs";
 
-const data = [davidData, someoneData];
+const data = [davidData, someoneData,CalvinData];
 export default data;
