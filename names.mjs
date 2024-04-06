0bdf17a229b333b5284af319b8181daeac27b0df
washialiamos <washialiamos018@gmail.com>
2024-04-06 12:21:30 +0300

    Add : add details in amos.mjs, add amos1data object in data list in name.mjs

--- names.mjs
+++ names.mjs
@@ -1,5 +1,6 @@
 import davidData from "./teamA/david.mjs";
+import amos1data from "./teamB/amos.mjs";
 import someoneData from "./teamB/someone.mjs";
 
-const data = [davidData, someoneData];
+const data = [davidData, someoneData, amos1data];
 export default data;
