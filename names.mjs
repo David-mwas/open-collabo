055da86df50ed7f873cc0dc9b7975c662c3fe824
James Otipa <otipajames2@gmail.com>
2024-04-06 12:21:48 +0300

    Add :add my detaails in otipa.mjs, add otipadata object in data list in name.mjs

--- names.mjs
+++ names.mjs
@@ -1,5 +1,5 @@
 import davidData from "./teamA/david.mjs";
 import someoneData from "./teamB/someone.mjs";
-
+import otipaData from "./teamB/otipa.mjs";
 const data = [davidData, someoneData];
 export default data;
