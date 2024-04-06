acae2b0795879681bca4bffa05b43a6c3e16515b
David-mwas <dmwas704@gmail.com>
2024-04-06 12:21:29 +0300

    Add : add my details in david.mjs, add daviddata object in data list in name.mjs

--- names.mjs
+++ names.mjs
@@ -1,5 +1,7 @@
 import davidData from "./teamA/david.mjs";
+import david1Data from "./teamB/david.mjs";
 import someoneData from "./teamB/someone.mjs";
 
-const data = [davidData, someoneData];
+const data = [davidData, someoneData, david1Data];
 export default data;
+s;
