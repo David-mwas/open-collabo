9cb3b86c485e1c34809cdb99c4da08f8902d37e0
Ibrahim <abdiwaliibrahimbulle@gmail.com>
2024-10-24 17:45:44 +0300

    added new contributer

---
+++ teamA/Ibrahim.mjs
@@ -0,0 +1,21 @@
+const ibrahimData = {
+    teamName: "A",
+    firstname: "Ibrahim",
+    lastname: "Bulle",
+    email: "ibrahmbulle@example.com",
+    role: "Student",
+    skills: ["HTML", "CSS", "JAVASCRIPT","C","C++","JAVA","PYTHON","KOTLIN","DJANGO"],
+    socials: [
+      {
+        git: "IbrahimBulle",
+      },
+      {
+        twitter: "Abdiwalbulle",
+      },
+    ],
+  };
+  
+  export default ibrahimData;
+ 
+  
+ 
